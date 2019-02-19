20690167dd7261963e83056818908676bf04aeb5
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-02-19 20:37:26 -0300

    Referenciando os posts do usuário

--- src/main/java/com/piresalexsandro/workshopmongo/dto/PostDTO.java
+++
@@ -1,5 +0,0 @@
-package com.piresalexsandro.workshopmongo.dto;
-
-public class PostDTO {
-
-}
--- src/main/java/com/piresalexsandro/workshopmongo/resource/User.java
+++ src/main/java/com/piresalexsandro/workshopmongo/resource/User.java
@@ -1,9 +1,14 @@
 package com.piresalexsandro.workshopmongo.resource;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.piresalexsandro.workshopmongo.domain.Post;
 
 @Document
 //@Document(Collection="user"
@@ -15,6 +20,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts =  new ArrayList<>();
 	
 	public User() {
 	}
@@ -48,6 +56,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
